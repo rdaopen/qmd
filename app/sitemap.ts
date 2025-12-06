d0bdb97629b50799d1d165e313448ed7b40a5e8c
RDA dev <94736945+mrrda1969@users.noreply.github.com>
2025-12-07 00:37:58 +0200

    fix invalid sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,21 @@
 import { MetadataRoute } from "next";
 
+const baseUrl =
+  process.env.NEXT_PUBLIC_BASE_URL || "https://qmd.everything-open.online";
+
 export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-        {
-            url: "/",
-            lastModified: new Date().toISOString(),
-            changeFrequency: "monthly",
-            priority: 1,
-        },
-        {
-            url: "/releases",
-            lastModified: new Date().toISOString(),
-            changeFrequency: "monthly",
-            priority: 0.5,
-        },
-    ];
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/releases`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
 }
